bd2397d3a492654415db51dce444b3a2ee50e7be
Mariana0908 <marianaospinahenao0908@gmail.com>
2025-11-13 01:00:07 -0500

    path

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -18,7 +18,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useAuth } from "../context/authContext";
 import { getAllUsers } from "../services/userService";
 
-const socket = io("http://3.131.85.236:5000");
+const socket = io(__API_URL__+"/ws/chat");
 
 const Chat = () => {
   const navigate = useNavigate();
